dbf42ab25e8f42faa0f7c9e6506f9cba26af02b5
zurly <e@zurly.lol>
2023-12-23 23:54:18 +0000

    fixed build error?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,41 @@
-// ./src/app/page.tsx
+"use client";
 
 import Countdown from "../components/tiles/Countdown";
 import Mail from "../components/tiles/Mail";
 import Time from "../components/tiles/Time";
 import Socials from "../components/tiles/Socials";
 import Spotify from "../components/tiles/Spotify";
-import Discord from "../components/tiles/Discord";  // Check this import statement
+import Discord from "../components/tiles/Discord";
 import Domain from "../components/tiles/Domain";
 import Image from "next/image";
+import { useDiscordData } from '../hooks/useDiscordData';
 
-interface HomeProps {
-  discordData: any;
-}
+const Home = () => {
+  const discordData = useDiscordData();
 
-const Home = ({ discordData }: HomeProps) => (
-  <main>
-    <div className="mx-4 flex items-center justify-center">
-      <div className="grid max-w-prose grid-cols-3 grid-rows-2 gap-3 md:gap-6">
-        <div className="col-span-3 row-span-2 flex items-center justify-center py-8 text-center">
-          <div className="mr-2">
-            <Image src="/logo.png" alt="Logo" width={40} height={40} />
+  return (
+    <main>
+      <div className="mx-4 flex items-center justify-center">
+        <div className="grid max-w-prose grid-cols-3 grid-rows-2 gap-3 md:gap-6">
+          <div className="col-span-3 row-span-2 flex items-center justify-center py-8 text-center">
+            <div className="mr-2">
+              <Image src="/logo.png" alt="Logo" width={40} height={40} />
+            </div>
+            <h1 className="mb-2 bg-gradient-to-tl from-white via-gray-400 to-pink-300 bg-300% bg-clip-text text-4xl font-bold text-transparent motion-safe:animate-gradient">
+              zurly.lol &bull; moani.ng
+            </h1>
           </div>
-          <h1 className="mb-2 bg-gradient-to-tl from-white via-gray-400 to-pink-300 bg-300% bg-clip-text text-4xl font-bold text-transparent motion-safe:animate-gradient">
-            zurly.lol &bull; moani.ng
-          </h1>
+          <Domain />
+          <Socials />
+          <Spotify />
+          <Time />
+          <Countdown />
+          <Mail />
+          {discordData && <Discord data={discordData} />}
         </div>
-        <Domain />
-        <Socials />
-        <Spotify />
-        <Time />
-        <Countdown />
-        <Mail />
-        <Discord data={discordData} />
       </div>
-    </div>
-  </main>
-);
+    </main>
+  );
+};
 
 export default Home;
---
+++ src/hooks/useDiscordData.ts
@@ -0,0 +1,33 @@
+// useDiscordData.ts
+import { useEffect as useReactEffect, useState as useReactState } from 'react';
+
+export function useDiscordData(useEffect = useReactEffect, useState = useReactState) {
+  const [discordData, setDiscordData] = useState(null);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await fetch("https://api.lanyard.rest/v1/users/745631824163766412");
+        const data = await response.json();
+        return data.data;
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        return null;
+      }
+    };
+
+    const fetchData = async () => {
+      const data = await fetchUserData();
+      setDiscordData(data);
+    };
+
+    fetchData();
+    const interval = setInterval(fetchData, 5000); // Fetch data every 5 seconds
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return discordData;
+}
