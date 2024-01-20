b4e558e4b3d62e646049791b7d3265d1a135c8a0
zurly <d@zurly.me>
2024-01-20 23:09:13 +0000

    Add speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
 import { inter } from "./fonts";
-import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -13,7 +13,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       </head>
       <body className={`${inter.className} bg-zinc-900 text-zinc-300`}>
         {children}
-        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
