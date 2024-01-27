bfaef8b48a2f860da003ea089dc3498a423dec03
zurly <d@zurly.me>
2024-01-27 13:23:22 +0000

    Refactor Spotify tile component

--- src/components/tiles/Spotify.tsx
+++ src/components/tiles/Spotify.tsx
@@ -55,53 +55,63 @@ const Home: NextPage = () => {
 
   if (data.spotify === null) {
     return (
-
-      <Link href="/spotify" className="col-span-3 row-span-2 rounded-2xl bg-black p-6 shadow md:col-span-2 md:p-8">
+      <Link href="/stats" className="col-span-3 row-span-2 rounded-2xl bg-black p-6 shadow md:col-span-2 md:p-8">
         <h2 className="mb-3 bg-gradient-to-tl from-zinc-500 to-zinc-300 bg-clip-text text-xl font-semibold text-transparent md:text-2xl">
           Spotify
-        </h2> <br />
+        </h2>{" "}
+        <br />
         Not listening to anything right now &gt;_&lt;
-      </Link> 
+      </Link>
     );
   }
 
-  const progress = ((new Date().getTime() - data.spotify.timestamps.start) / (data.spotify.timestamps.end - data.spotify.timestamps.start)) * 100;
+  const progress =
+    ((new Date().getTime() - data.spotify.timestamps.start) /
+      (data.spotify.timestamps.end - data.spotify.timestamps.start)) *
+    100;
 
   return (
     <div
-      className="col-span-3 row-span-2 rounded-2xl bg-teal-950 p-6 shadow md:col-span-2 md:p-8 relative"
+      className="relative col-span-3 row-span-2 rounded-2xl bg-teal-950 p-6 shadow md:col-span-2 md:p-8"
       style={{
         backgroundImage: `url(${data.spotify.album_art_url})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}
     >
-      <div
-        className="backdrop-filter backdrop-blur-md rounded-2xl p-6"
-        style={{
-          backgroundColor: "rgba(0, 0, 0, 0.3)",
-          position: "absolute",
-          zIndex: 1,
-          top: 0,
-          bottom: 0,
-          left: 0,
-          right: 0,
-        }}
-      >
-        <h2 className="mb-3 bg-gradient-to-tl text-white bg-clip-text text-xl font-semibold text-transparent md:text-2xl">
-          Spotify
-        </h2> <br />
-        <p><strong>Listening to: {data.spotify.song}</strong></p>
-        <p><strong>By: {data.spotify.artist}</strong></p>
-        <div className="absolute bottom-0 left-0 right-0 h-[0.35rem] rounded-full overflow-hidden">
-          <div
-            className="bg-gray-300 h-[0.35rem] rounded-full overflow-hidden"
-            style={{
-              width: `${progress > 100 ? 100 : progress}%`,
-            }}
-          />
+      <Link href="/stats">
+        <div
+          className="rounded-2xl p-6 backdrop-blur-md backdrop-filter"
+          style={{
+            backgroundColor: "rgba(0, 0, 0, 0.3)",
+            position: "absolute",
+            zIndex: 1,
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+          }}
+        >
+          <h2 className="mb-3 bg-gradient-to-tl bg-clip-text text-xl font-semibold text-transparent text-white md:text-2xl">
+            Spotify
+          </h2>{" "}
+          <br />
+          <p>
+            <strong>Listening to: {data.spotify.song}</strong>
+          </p>
+          <p>
+            <strong>By: {data.spotify.artist}</strong>
+          </p>
+          <div className="absolute bottom-0 left-0 right-0 h-[0.35rem] overflow-hidden rounded-full">
+            <div
+              className="h-[0.35rem] overflow-hidden rounded-full bg-gray-300"
+              style={{
+                width: `${progress > 100 ? 100 : progress}%`,
+              }}
+            />
+          </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 };
