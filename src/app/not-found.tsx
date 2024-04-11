47862428f6fabd6cb534cb0db501115498a8318a
dk-e <d@zurly.me>
2024-04-11 13:23:54 +0100

    bun

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,21 +2,15 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <>
-      <title>404 · zurly</title>
+    <main className="mx-auto max-w-3xl space-y-2 px-6 pb-40 pt-16">
+      <p className="font-title text-3xl">404</p>
+      <p>Sorry, I couldn&apos;t locate that page for ya</p>
 
-      <div className="mx-4 mt-8 flex items-center justify-center h-screen">
-        <div className="w-full max-w-prose text-center">
-          <h1 className="mb-8 text-4xl font-bold">404</h1>
-          <div className="whitespace-pre-wrap text-left font-serif text-sm leading-6">
-          </div>
-          <div className="mt-8">
-            <Link href="/">
-              <span className="text-blue-500 hover:underline">Back to Home</span>
-            </Link>
-          </div>
-        </div>
+      <div>
+        <Link href="/" className="rounded-full bg-pink-500 px-2 py-1">
+          ↖ home
+        </Link>
       </div>
-    </>
+    </main>
   );
 }
